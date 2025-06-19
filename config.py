5110c60ce3bf4039cf3e8dc7fc277f768ae4620c
icryptoboss <141033841+icryptoboss@users.noreply.github.com>
2025-06-19 12:43:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
 class Config(object):
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    AUTH_USER = os.environ.get('AUTH_USERS', '7602994049').split(',')
+BOT_TOKEN = environ.get("BOT_TOKEN", "7874207255:AAHpnRakHABD67whsLmNWJNel69FtSRqQf0")
+API_ID = int(environ.get("API_ID", "25679601"))
+API_HASH = environ.get("API_HASH", "105e12ce694578ac241c66d267caee87")
+AUTH_USER = os.environ.get('AUTH_USERS', '1391520393').split(',')
     AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
     HOST = "https://drm-api-six.vercel.app"
-    CREDIT = " 𝙎𝘼𝙄𝙉𝙄 𝘽𝙊𝙏𝙎"
+    CREDIT = " rahul"
